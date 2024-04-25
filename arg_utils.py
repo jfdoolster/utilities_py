d6eda6e34b5cb6282d1cab78164eb5528bc62d2d
jdooley <jfdoolster@gmail.com>
2024-04-25 13:18:37 -0600

    update

---
+++ arg_utils.py
@@ -0,0 +1,11 @@
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-f', '--file', required=True, type=str, help='path to element file')
+
+    argdict = vars(parser.parse_args())
+    print(argdict)
--- fileio_utils.py
+++ fileio_utils.py
@@ -2,29 +2,32 @@
 import os
 import glob
 
-def unix_path(path: str) -> str:
+def clean_path(path: str) -> str:
     _path = path.replace('$HOME','~')
+    _path = os.path.expanduser(_path)
+    return _path
+
+def unix_path(path: str) -> str:
+    _path = clean_path(path)
     return os.path.normpath(_path).replace(os.sep, '/')
 
 def absolute_path(path: str, unix_style=True) -> str:
-    _path = path.replace('$HOME','~')
-    _path = os.path.expanduser(_path)
+    _path = clean_path(path)
+    _path = os.path.abspath(_path)
     if unix_style:
-        return unix_path(os.path.abspath(_path))
-    return os.path.abspath(_path)
+        return unix_path(_path)
+    return _path
 
 def get_directory_files(directory: str, wildcard='*'):
     dir = absolute_path(directory)
-
     if not os.path.isdir(dir):
         print(f"{dir:s} does not exist")
         return list()
-
     return sorted(glob.glob(f"{dir:s}/{wildcard}"))
 
 if __name__ == "__main__":
-    path = '$HOME'
+    path = '../../Documents/jfd_logs'
     print(unix_path(path))
     print(absolute_path(path, False))
     print(absolute_path(path, True))
-    print(get_directory_files(path, wildcard="*.png"))
+    print(get_directory_files(path, wildcard="*"))
