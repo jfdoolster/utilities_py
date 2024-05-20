3b30e02980c117e7e26e87b0dc52648326a11730
J. F. Dooley <jfdoolster@gmail.com>
2024-05-20 12:52:16 -0600

    autocommit: jdooley@based (24/05/20 12:52:16)

--- plt_utils.py
+++ plt_utils.py
@@ -7,6 +7,26 @@ import matplotlib.pyplot as plt
 from itertools import cycle
 from log_utils import WARNING, INFO
 
+def set_custom_rcparams(grid=True):
+    """
+    custom rcparams
+    """
+    plt.rcParams['axes.grid'] = grid
+    #plt.rcParams['image.cmap'] = 'viridis_r'
+    #plt.rc('font', size=12)
+    ## Set the axes title font size
+    #plt.rc('axes', titlesize=16)
+    ## Set the axes labels font size
+    #plt.rc('axes', labelsize=14)
+    ## Set the font size for x tick labels
+    #plt.rc('xtick', labelsize=12)
+    ## Set the font size for y tick labels
+    #plt.rc('ytick', labelsize=12)
+    ## Set the legend font size
+    #plt.rc('legend', fontsize=10)
+    ## Set the font size of the figure title
+    #plt.rc('figure', titlesize=16)
+
 def make_iterator(prop_cycle:list):
     return cycle(prop_cycle)
 
