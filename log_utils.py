2cd2ac3490dc92597b845a0be7c27d99eca1b32b
J. F. Dooley <jfdoolster@gmail.com>
2024-05-21 11:49:32 -0600

    update logging and savefigs

--- log_utils.py
+++ log_utils.py
@@ -17,7 +17,7 @@ def escape_ansi(line):
     ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
     return ansi_escape.sub('', line)
 
-def DISPLAY(hdr:str, msg:str, fname:bool, slen=6):
+def DISPLAY(hdr:str, msg:str, show_func:bool, slen=6):
     while len(hdr) < slen+1:
         hdr+=' '
     if len(hdr) > slen:
@@ -29,27 +29,33 @@ def DISPLAY(hdr:str, msg:str, fname:bool, slen=6):
     elif 'pass' in hdr.lower(): hdr=str(bcolors.OKGREEN + hdr.upper() + bcolors.ENDC)
     else: hdr=str(hdr.upper() + bcolors.ENDC)
 
-    PRINT = f"{hdr:{slen}s}: {msg:s}"
-    if (fname):
-        FNAME = inspect.stack()[2].function
-        if (FNAME.lower() not in ['<module>']):
-            FNAME = bcolors.BOLD  + f"{FNAME:s}()"+ bcolors.ENDC
-            PRINT = f"{hdr:{slen}s}: [{FNAME:s}] {msg:s}"
+    HDR = f"{hdr:{slen}s}"
+    if hdr.strip() != "":
+        HDR = HDR+":"
+
+    FNAME = ""
+    if show_func:
+        stack_func = inspect.stack()[2].function
+        if (stack_func.lower() not in ['<module>']):
+            FNAME = "["+bcolors.BOLD  + f"{stack_func:s}()"+ bcolors.ENDC+"]"
+    PRINT = f"{HDR:s} {FNAME:s} {msg:s}"
     print(PRINT)
     return escape_ansi(PRINT)
 
-def INFO(msg:str="", hdr:str='INFO', show_func:bool=True):
-    return DISPLAY(hdr, msg, fname=show_func)
+def INFO(msg:str="", hdr:str='INFO', show_func:bool=False):
+    return DISPLAY(hdr, msg, show_func=show_func)
 
 def WARNING(msg:str="", show_func:bool=True):
-    return DISPLAY('WARN', msg, fname=show_func)
+    return DISPLAY('WARN', msg, show_func=show_func)
 
 def ERROR(msg:str="", show_func:bool=True, force_exit:bool=True):
-    s = DISPLAY('FAIL', msg, fname=show_func)
+    s = DISPLAY('FAIL', msg, show_func=show_func)
     if force_exit:
-        print("Exiting...")
+        print("Exiting...\n")
         sys.exit()
     return s
 
-def SUCCESS(msg:str="", show_func:bool=True):
-    return DISPLAY('PASS', msg, fname=show_func)
+def SUCCESS(msg:str="", show_func:bool=False, newline:bool=True):
+    if newline:
+        if msg[-1] != '\n': msg = msg + "\n"
+    return DISPLAY('PASS', msg, show_func=show_func)
